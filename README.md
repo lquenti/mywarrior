cdd18077d49cfa1ebe1644de73d70a0b12f1d8f8
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2024-11-21 16:13:32 +0100

    implement proper arg parser

---
+++ README.md
@@ -0,0 +1,3 @@
+## single header libs used
+- [json](https://github.com/nlohmann/json)
+- [argparse](https://github.com/p-ranav/argparse)
--- main.cc
+++ main.cc
@@ -9,6 +9,7 @@
 #include <string>
 #include <thread>
 
+#include "argparse.hpp"
 #include "json.hpp"
 
 // stop == stopped by SIGINT or end of timer
@@ -92,20 +93,11 @@ void signal_handler(int signal) {
   std::cout << std::endl << "Press enter to exit!" << std::endl;
 }
 
-int main(int argc, char **argv) {
+int track_main(std::uint64_t pomodoro_count) {
   std::signal(SIGINT, signal_handler);
-  /* for now pomodori */
-  if (argc != 2) {
-    std::cerr << "USAGE: mywarrior (AMOUNT OF POMODORI)" << std::endl;
-    return EXIT_FAILURE;
-  }
-  std::uint64_t pomodoro_count{
-    static_cast<std::uint64_t>(std::strtol(argv[1], nullptr, 10))
-  };
   debug_print("Pomodoro count: ", pomodoro_count);
 
-  //std::uint64_t total_seconds{pomodoro_count*60*25};
-  std::uint64_t total_seconds{3};
+  std::uint64_t total_seconds{pomodoro_count*60*25};
   debug_print("Total seconds: ", total_seconds);
   std::cout << "Enter to stop early" << std::endl;
 
@@ -133,4 +125,47 @@ int main(int argc, char **argv) {
   ofs << json_str << std::endl;
   ofs.close();
   debug_print("end");
+  return EXIT_SUCCESS;
+}
+
+void report_main() {
+  std::cout << "to be implemented..." << std::endl;
+}
+
+int main(int argc, char **argv) {
+  argparse::ArgumentParser program("mywarrior", "0.0.1");
+
+  argparse::ArgumentParser track_command("track");
+  track_command.add_description("Tracks pomodori");
+  track_command.add_argument("pomodori")
+    .help("The amount of pomodori (25min) done in a row")
+    .scan<'i', int>();
+
+  argparse::ArgumentParser report_command("report");
+  report_command.add_description("provides report of recent work");
+
+  program.add_subparser(track_command);
+  program.add_subparser(report_command);
+
+  try {
+    program.parse_args(argc, argv);
+  } catch (const std::exception &err) {
+    std::cerr << err.what() << std::endl;
+    std::cerr << program;
+    return EXIT_FAILURE;
+  }
+
+  if (program.is_subcommand_used("track")) {
+    debug_print("Starting Track");
+    int pomodori{track_command.get<int>("pomodori")};
+    track_main(pomodori);
+    return EXIT_SUCCESS;
+  } else if (program.is_subcommand_used("report")) {
+    debug_print("Starting Report");
+    report_main();
+    return EXIT_SUCCESS;
+  } else {
+    std::cerr << program << std::endl;
+    return EXIT_FAILURE;
+  }
 }
